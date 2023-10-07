00b472c56a77208ce8597a6df9713c2577188c07
ericWang <eynyseric520@gmail.com>
2023-10-07 17:21:11 +0800

    Upgrade project to spring boot 3

--- src/main/java/tw/com/ispan/eeit48/springSecurity/config/SecurityConfig.java
+++ src/main/java/tw/com/ispan/eeit48/springSecurity/config/SecurityConfig.java
@@ -10,23 +10,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
 import tw.com.ispan.eeit48.mainFunction.service.AuthService;
 import tw.com.ispan.eeit48.springSecurity.filter.JWTAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
-	
     private final AuthService authService;
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
@@ -40,8 +35,7 @@ public class SecurityConfig{
     }
 
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) 
-             throws Exception {
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
@@ -55,22 +49,20 @@ public class SecurityConfig{
 	      return provider;
 	}
 
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http,AuthenticationManagerBuilder auth) throws Exception {
         http
                 // 因為有 JWT，所以可以防範 csrf 攻擊，因此將其 disable
                 .csrf(csrf -> csrf.disable())
                 // 授權認證
-                .authorizeHttpRequests(authorize -> {
+                .authorizeHttpRequests(authorize ->
      			   authorize
-     			// 不需要被認證的API
+     			   // 不需要被認證的API
                    .requestMatchers("/login").permitAll()
                    .requestMatchers("/ecPay").permitAll()
                    // 必須要有特殊權限才可以訪問的API
                    .requestMatchers("/views/analyze").hasRole("BOSS")
-                   // Spring 5.3後 基於效能理由 路徑解析從 AntPathMatcher 改為 PathPatternParser
-                   // 要使用 AntPathMatcher 需特別指定 AntPathRequestMatcher
+                   // Spring 5.3後 基於效能理由 路徑解析從 AntPathMatcher 改為 PathPatternParser (若要使用 AntPathMatcher 需特別指定 AntPathRequestMatcher)
                    .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.OPTIONS,"/**")).permitAll()
                    .requestMatchers(AntPathRequestMatcher.antMatcher("/**/*.html")).permitAll()
                    .requestMatchers(AntPathRequestMatcher.antMatcher("/**/*.css")).permitAll()
@@ -85,24 +77,23 @@ public class SecurityConfig{
                    .requestMatchers(AntPathRequestMatcher.antMatcher("/favicon.ico")).permitAll()
                    .requestMatchers("/error").permitAll()
                    // 其他頁面必須要有驗證才能訪問
-                   .anyRequest().authenticated();
-                   // 因為是 JWT 機制，所以不應該產生 Session，因此將 Session 建立取消
-                });
-                // 不需要被認證的API
-        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                   .anyRequest().authenticated()
+                );
+
+        http.sessionManagement(management ->
+                        // 因為是 JWT 機制，所以不應該產生 Session，因此將 Session 建立取消
+                        management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                /**
+                 * 因為每個受保護的 API 請求都需要去檢查其 Header 上面的 token 是否合法，因此添加了一個自定義的 jwt filter 在 UsernamePasswordAuthenticationFilter 前面，
+                 * 這樣可以避免用原生 Spring Security 的方式幫我們做檢查，而是採用我們自定義的方式
+                 */
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         http.authenticationProvider(daoAuthenticationProvider());
+
+        // Configuring Spring Security to disable the X-Frame-Options header, which allows web pages to be displayed in iframes without restrictions.
         http.headers(headers->headers.frameOptions(frameOptions->frameOptions.disable()));
+
         return http.build();
     }
-//    改在 HttpSecurity 設定
-    // Configure paths and requests that should be ignored by Spring Security ================================
-//    @Bean
-//    WebSecurityCustomizer webSecurityCustomizer() {
-//    	return (web)->web.ignoring()
-//              .requestMatchers(HttpMethod.OPTIONS, "/**")
-////              // allow anonymous resource requests
-//              .requestMatchers("/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png")
-////              // allow websocket
-//              .requestMatchers("/ws/**");
-//    }
 }
